54be1c27142ddc749b1e795293a3b4e6229b7546
Quy33 <87293639+Quy33@users.noreply.github.com>
2021-07-17 15:00:37 +0700

    Initial Commit

---
+++ Tasks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tasks
+//
+//  Created by Mcrew Tech on 17/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
